b195cdc7cda4b41ed1f1c3e67e709fea52a997eb
Tommy Mapp <61087797+tommymapp@users.noreply.github.com>
2026-07-29 13:45:05 +0100

    [IDDS-435] Update down revision for new migration to stop conflict (#124)

--- app/db/migrations/versions/0abbfeb39330_idds_435_update_public_bodies.py
+++ app/db/migrations/versions/0abbfeb39330_idds_435_update_public_bodies.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "0abbfeb39330"
-down_revision: Union[str, None] = "fc72a3f74810"
+down_revision: Union[str, None] = "f1a2b3c4d5e6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
